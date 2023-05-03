9b30dc4921b702300d9cfeaa828efc1ff811411f
motypes <mo@openfren.com>
2023-05-03 15:33:15 +0800

    initial weather component complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className='text-6xl'>
-      <h1>GPT 4 Weather App</h1>
+    <main className="min-h-screen bg-gradient-to-br from-[#394F68]
+     to-[#183B7E] p-10 flex flex-col justify-center items-center ">
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">Powered by GPT-4</Subtitle>
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          {/* City Picker */}
+        </Card>
+      </Card>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    
+    // Tremor modules
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}', 
   ],
   theme: {
     extend: {
