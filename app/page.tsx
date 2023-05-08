de9a37c07a1f13a20bf0a271708acf6c658af0a9
motypes <mo@openfren.com>
2023-05-08 20:16:03 +0800

    added city selector

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Inter } from "next/font/google";
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
+import CityPicker from "@/components/CityPicker";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function Home() {
         <Divider className="my-10" />
         <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
           {/* City Picker */}
+          <CityPicker></CityPicker>
         </Card>
       </Card>
     </main>
